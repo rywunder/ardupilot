0d8f9667f67de2ff39e676b433edbee9784e9b75
Ryan Wunderly <rywunder@umich.edu>
2021-03-02 17:56:51 -0800

    cleanup

--- libraries/AC_AttitudeControl/AC_AttitudeControl_Multi.cpp
+++ libraries/AC_AttitudeControl/AC_AttitudeControl_Multi.cpp
@@ -337,23 +337,11 @@ void AC_AttitudeControl_Multi::rate_controller_run()
 
     Vector3f gyro_latest = _ahrs.get_gyro_latest();
 
+    _motors.set_roll(get_rate_roll_pid().update_all(_rate_target_ang_vel.x, gyro_latest.x, _motors.limit.roll) + _actuator_sysid.x);
+    _motors.set_roll_ff(get_rate_roll_pid().get_ff());
 
-    // TODO: Add boolean
-    if(true)
-    {
-        // Call the AC heli code here 
-        _motors.set_pitch(get_rate_pitch_adap().update(AP::ins().get_loop_rate_hz(), _rate_target_ang_vel.y, gyro_latest.y));
-        _motors.set_roll(get_rate_roll_adap().update(AP::ins().get_loop_rate_hz(), _rate_target_ang_vel.x, gyro_latest.x));
-    }
-    else
-    {
-        _motors.set_pitch(get_rate_pitch_pid().update_all(_rate_target_ang_vel.y, gyro_latest.y, _motors.limit.pitch) + _actuator_sysid.y);
-        _motors.set_pitch_ff(get_rate_pitch_pid().get_ff());
-
-        _motors.set_roll(get_rate_roll_pid().update_all(_rate_target_ang_vel.x, gyro_latest.x, _motors.limit.roll) + _actuator_sysid.x);
-        _motors.set_roll_ff(get_rate_roll_pid().get_ff());
-    }
-
+    _motors.set_pitch(get_rate_pitch_pid().update_all(_rate_target_ang_vel.y, gyro_latest.y, _motors.limit.pitch) + _actuator_sysid.y);
+    _motors.set_pitch_ff(get_rate_pitch_pid().get_ff());
 
     _motors.set_yaw(get_rate_yaw_pid().update_all(_rate_target_ang_vel.z, gyro_latest.z, _motors.limit.yaw) + _actuator_sysid.z);
     _motors.set_yaw_ff(get_rate_yaw_pid().get_ff()*_feedforward_scalar);
